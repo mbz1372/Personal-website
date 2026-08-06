25e3ecee5f32e010fd771785182814493a2ac943
Mohammad Bagher Zolfaghari <81497912+mbz1372@users.noreply.github.com>
2026-08-06 18:41:10 +0330

    Remove legacy reactive layer from redesigned homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import ExperienceEnhancer from './experience-enhancer';
 import Portfolio from './portfolio';
 
 export default function HomePage() {
-  return (
-    <>
-      <Portfolio />
-      <ExperienceEnhancer />
-    </>
-  );
+  return <Portfolio />;
 }
